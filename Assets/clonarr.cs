aca954256cf5cc8a1a7de7a59e6f0a0f10b45ad5
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-05-10 08:59:52 -0300

    Initial commit

---
+++ Assets/clonarr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clonarr : MonoBehaviour
+{
+    public GameObject cubito;
+    public int grados;
+    public int cant;
+
+    public void cloneobject()
+    {
+        GameObject clon;
+        for(int i= 0; i<cant; i++) { 
+        clon = Instantiate(cubito);
+        clon.transform.Rotate(i, grados * i, 0);
+        clon.transform.Translate(i, 5*i, 0);
+            //Destroy(clon, 1);
+        }
+    }
+}
